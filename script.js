086a8ca90406cb7b3059b4405828532d7b58f329
Vicky <karthikvignesh3@gmail.com>
2022-06-01 18:38:39 +0530

    task

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      margin: 10,
+      loop: true,
+      nav: true,
+      rtl:false,
+      responsiveClass:true,
+      dots: false,
+    });
+  });
